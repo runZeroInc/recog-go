69ea6012a2b2a5bacada62f376f4702e2193aa67
HD Moore <x@hdm.io>
2020-02-09 13:36:55 -0600

    recog updates

--- pkg/nition/update_recog.sh
+++ pkg/nition/update_recog.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-(cd recog && git commit -a -m "fingerprint updates") && \
+(cd recog && git commit -a -m "fingerprint updates")
+
 git add recog && git commit -a -m "recog updates" && go generate ./... && git commit -a -m "regenerate resources"
